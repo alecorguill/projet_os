13ad357c139cb90431dc0ebad6702cb9012d3fc7
LE DILY Hugo <hledily@avoranfix.pedago.ipb.fr>
2017-05-02 16:27:11 +0200

    mutex dummies

--- src/thread.c
+++ src/thread.c
@@ -163,16 +163,27 @@ int thread_mutex_init(thread_mutex_t *mutex){
 	
 	
 	
-	
+	return 0;
 }
 
 
 
 
 
-int thread_mutex_destroy(thread_mutex_t *mutex);
-int thread_mutex_lock(thread_mutex_t *mutex);
-int thread_mutex_unlock(thread_mutex_t *mutex);
+int thread_mutex_destroy(thread_mutex_t *mutex){
+	
+	return 0;
+}
+
+int thread_mutex_lock(thread_mutex_t *mutex){
+	
+	return 0;
+}
+
+int thread_mutex_unlock(thread_mutex_t *mutex){
+	
+	return 0;
+}
 
 
 
